69c73b479c6c02e2f860e8a6b9c8d910e21e3e9c
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-22 17:10:57 +1000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios"); 
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key":"c6946c82-5e82-453b-b866-aae291223b54"}}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
+
