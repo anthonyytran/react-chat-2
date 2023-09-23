ec3121e4e232028f81760a1c17f6f4c89e2c059b
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-23 15:48:40 +1000

    multi chat socket

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <> chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '85df2d4b-26ae-4a07-974f-d3659261cee6',
+        props.user.username, 
+        props.user.secret
+        )
+    return (
+    <div style={{ height: '100vh' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>
+    )
 }
 
 export default ChatsPage
